2d848a1384776774d2d50754c0a607f33f9bb474
victoriarivera18 <victoriaeugenia2018@gmail.com>
2020-06-10 23:59:42 -0500

    main function

---
+++ mpq-main.cpp
@@ -0,0 +1,19 @@
+// V. Rivera Casanova
+// CSCE 221
+// Leyk 199
+
+# include <iostream>
+# include <stdexcept>
+# include <cmath>
+# include <vector>
+using namespace std;
+
+# include "LinkedList-imp.h"
+# include "Vector-imp.h"
+// definition of (key, value) pair that makes up the mpq
+
+int main()
+{
+    return 0;
+}
+
