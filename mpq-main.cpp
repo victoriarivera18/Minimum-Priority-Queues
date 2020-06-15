06870946408e2d852b3f0463beb61b38e885a4c2
victoriarivera18 <victoriaeugenia2018@gmail.com>
2020-06-15 15:47:02 -0500

    Deleted excessive comments and have checked test files for accuracy and if they are running correctly. Only SetSize1000 and SetSize100000 text files are giving segmentation faults.

--- mpq-main.cpp
+++ mpq-main.cpp
@@ -19,96 +19,24 @@ using namespace std;
 // definition of (key, value) pair that makes up the mpq
 
 int main()
-{   
-    /*MinPriorityQueue<int, int> my_queue;
-
-    LinkedList<int, int> my_list;
-    if (my_list.is_empty()){
-        cout << "Adding new items into list" << endl;
-        my_list.insert(2, 4);
-        my_list.insert(-1, 4);
-        my_list.insert(8, 3);
-        my_list.insert(-5, 4);
-        my_list.insert(3, 4);
-        cout << my_list.remove_min() << endl;
-    } else {
-        cout << "List not empty"<< endl;
-    }
-
-    if (my_list.is_empty()){
-        cout << "Adding new items into list" << endl;
-        my_list.insert(2, 4);
-        my_list.insert(-1, 4);
-        my_list.insert(5, 4);
-        my_list.insert(3, 4);
-    } else {
-        cout << "List not empty"<< endl;
-    }
-
-
-    if (my_queue.is_empty()){
-        cout << "Adding new pairs into vector" << endl;
-        my_queue.insert(2, 4);
-        my_queue.insert(-1, 4);
-        my_queue.insert(5, 4);
-        my_queue.insert(3, 4);
-        cout << "Size: " <<  my_queue.mpq.size() << endl;
-    } else {
-        cout << "List not empty"<< endl;
-    }
-
-    cout << "Minimum: " << my_queue.remove_min() << endl;
-
-    if (my_queue.is_empty()){
-        cout << "Adding new pairs into vector" << endl;
-        my_queue.insert(2, 4);
-        my_queue.insert(-1, 4);
-        my_queue.insert(5, 4);
-        my_queue.insert(3, 4);
-        cout << "Size: " <<  my_queue.mpq.size() << endl;
-    } else {
-        cout << "List not empty"<< endl;
-    } */
-    /* BinaryHeap<int> intHeap;
-    BinaryHeap<CPU_Job> myHeap;
-
-    intHeap.insert(1);
-    intHeap.insert(4);
-    intHeap.insert(7);
-    intHeap.insert(-2);
-    intHeap.insert(5);
-    intHeap.insert(3);
-
-    try {
-        cout << intHeap.remove_min() << endl;
-        cout << intHeap.remove_min() << endl;
-        cout << intHeap.remove_min() << endl;
-        cout << intHeap.remove_min() << endl;
-        cout << intHeap.remove_min() << endl;
-        cout << intHeap.remove_min() << endl;
-        // cout << intHeap.remove_min() << endl; invokes runtime_error()
-
-    } catch(EmptyTree){
-        cout << "Tree is empty!" << endl;
-    } */
-
+{
     //need to create struct
     //insert into binary heap of CPU_Jobs
 
     //reading from file also
-    BinaryHeap<CPU_Job> myHeap4;
-    BinaryHeap<CPU_Job> myHeap10;
-    BinaryHeap<CPU_Job> myHeap100;
-    BinaryHeap<CPU_Job> myHeap1000;
+    BinaryHeap<CPU_Job> myHeap4; // works
+    BinaryHeap<CPU_Job> myHeap10; // works
+    BinaryHeap<CPU_Job> myHeap100; // works
+    BinaryHeap<CPU_Job> myHeap1000; //works
     BinaryHeap<CPU_Job> myHeap10000;
     BinaryHeap<CPU_Job> myHeap100000;
 
     string file4 = "SetSize4.txt";
     string file10 = "SetSize10.txt";
     string file100 = "SetSize100.txt";
-    string file1000 = "SetSize10000.txt";
+    string file1000 = "SetSize1000.txt";
     string file10000 = "SetSize10000.txt";
-    string file100000 = "SetSize10000.txt";
+    string file100000 = "SetSize100000.txt";
 
     ifstream ifs(file4);
     //ifstream ifs2(file10);
@@ -121,7 +49,7 @@ int main()
     string line;
     stringstream ss;
     
-    /*while(!ifs.eof()){ // only going into loop 1 time when doing 10
+    while(!ifs.eof()){ // only going into loop 1 time when doing 10
         getline(ifs, line);
         ss << line;
         ss >> id >> len >> prior;
@@ -164,7 +92,7 @@ int main()
         ss >> id >> len >> prior;
         CPU_Job temp3(id, len, prior);
         myHeap1000.insert(temp3); // inserting job into heap
-    }*/
+    }
 
     ss.clear();
     ifs.close();
@@ -189,6 +117,8 @@ int main()
         CPU_Job temp5(id, len, prior);
         myHeap100000.insert(temp5); // inserting job into heap
     }
+    ss.clear();
+    ifs.clear();
     ifs.close();
 
     try {
@@ -206,12 +136,12 @@ int main()
             cout << myHeap100.remove_min();
         }
 
+*/
+        while(!myHeap100000.is_empty()){
+            cout << myHeap100000.remove_min();
+        }
 
-        while(!myHeap1000.is_empty()){
-            cout << myHeap1000.remove_min();
-        }*/
-
-        cout << myHeap10000.getSize()<< endl;;
+        //cout << myHeap10000.getSize()<< endl;;
   
 
     } catch(EmptyTree){
